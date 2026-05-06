5680ba0a67bc2bc0d7f01e352cf0edb804452406
Nguyen Kien <maukientl@gmail.com>
2026-05-06 09:53:28 +0700

    ci created failed test

--- src/test/java/com/lab/ShippingCalculatorTest.java
+++ src/test/java/com/lab/ShippingCalculatorTest.java
@@ -14,7 +14,7 @@ public class ShippingCalculatorTest {
 
     @Test
     void testExpress() {
-        assertEquals(45000.0, calc.calculate(5, "EXPRESS"));
+        assertEquals(45000.0, calc.calculate(6, "EXPRESS"));
     }
 
     @Test
